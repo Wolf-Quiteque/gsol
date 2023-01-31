0d861a87323b06e804f8b65ac88530cd1a73f4e6
Wolf-Quiteque <marcioqui3@gmail.com>
2023-01-31 19:47:31 +0200

    jk

--- pages/index.js
+++ pages/index.js
@@ -185,7 +185,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>G-Lab</title>
+        <title>G-LAB</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
